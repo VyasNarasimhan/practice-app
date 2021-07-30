00f18ad7ccffcdd17ca6de524d3dd84f603e3b5b
Vyas Narasimhan <reach.vyas.2003@gmail.com>
2021-07-30 09:21:46 -0400

    Display url instead of just ip

--- runner.py
+++ runner.py
@@ -11,7 +11,7 @@ for line in env.readlines():
         parts = line.split(': ')
         result = os.popen("ipconfig | findstr \"192.168.0\"").read()
         ipv4 = result.split("\n")[0].strip()[-13:]
-        print(ipv4)
+        print(f'http://{ipv4}:4200')
         parts[1] = '\'http://' + ipv4 + ':4201\'\n'
         lines.append(parts[0] + ': ' + parts[1])
 with open("D:\\practice-app\\src\\environments\\environment.ts", 'w') as dest:
