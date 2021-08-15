cab85c4965e6af46dfbf5939cac420d6bb886741
Vyas Narasimhan <reach.vyas.2003@gmail.com>
2021-08-14 22:14:21 -0400

    get param can be upper or lowercase

--- server/practice/routes/logFile.ts
+++ server/practice/routes/logFile.ts
@@ -5,7 +5,8 @@ export const router: Router = Router();
 
 router.post('/writeToFile', async (req: Request, res: Response, next: NextFunction) => {
   console.log('Inside writeToFile post');
-  fs.writeFile('./server/songslogs/' + req.body.name + 'SongsLog.json', JSON.stringify(req.body.json), (err: any) => {
+  const name = req.body.name.charAt(0).toUpperCase() + req.body.name.slice(1).toLowerCase();
+  fs.writeFile('./server/songslogs/' + name + 'SongsLog.json', JSON.stringify(req.body.json), (err: any) => {
     if (err) {
       console.log(err);
     } else {
@@ -16,8 +17,10 @@ router.post('/writeToFile', async (req: Request, res: Response, next: NextFuncti
 
 router.get('/getFile/:name', async (req: Request, res: Response, next: NextFunction) => {
   console.log('Inside getFile get');
-  if (fs.existsSync('./server/songslogs/' + req.params.name + 'SongsLog.json')) {
-    fs.readFile('./server/songslogs/' + req.params.name + 'SongsLog.json', 'utf8', (err: any, data: any) => {
+  const name = req.params.name.charAt(0).toUpperCase() + req.params.name.slice(1).toLowerCase();
+  if (fs.existsSync('./server/songslogs/' + name + 'SongsLog.json')) {
+    // tslint:disable-next-line: max-line-length
+    fs.readFile('./server/songslogs/' + name + 'SongsLog.json', 'utf8', (err: any, data: any) => {
       if (err) {
         console.log(err);
       } else {
