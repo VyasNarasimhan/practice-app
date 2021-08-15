784b0f77d79dcd8ab85f47bd26425d2845a1ea91
Vyas Narasimhan <reach.vyas.2003@gmail.com>
2021-08-14 21:59:54 -0400

    touch

--- AddUser.py
+++ AddUser.py
@@ -1,6 +1,6 @@
 import sys
 
-print(sys.argv[0])
-newFile = open('server\\songslogs\\' + sys.argv[0] + 'SongsLog.json', 'w')
+print(sys.argv[1])
+newFile = open('server\\songslogs\\' + sys.argv[1] + 'SongsLog.json', 'w')
 newFile.write('[]')
 newFile.close()
