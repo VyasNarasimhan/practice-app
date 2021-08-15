ee2b61ed0c5cb5f90ba1a32ed97486dbe73d3a1b
Ubuntu <ubuntu@ip-172-31-11-24.ec2.internal>
2021-08-15 02:06:14 +0000

    AddUser works on ubuntu

--- AddUser.py
+++ AddUser.py
@@ -1,6 +1,7 @@
 import sys
+import os
 
 print(sys.argv[1])
-newFile = open('server\\songslogs\\' + sys.argv[1] + 'SongsLog.json', 'w')
+newFile = open('/home/ubuntu/practice-app/server/songslogs/' + sys.argv[1] + 'SongsLog.json', 'w')
 newFile.write('[]')
 newFile.close()
